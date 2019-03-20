945a8b7afd34a917e6b9a371ac4e2cca8c0629b0
Thomas Roman <thomasroman32@yahoo.com>
2019-03-19 20:58:54 -0500

    Registration and login fields are working visually

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react'
+import Paper from '@material-ui/core/Paper'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
-const Register = () => (
-    <div>
-        This is the Register page
-    </div>
-)
+class Register extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            email: '', 
+            login: '',
+            password: '',
+            password2: ''
+            
+        }
+    }
+    render() {
+        return (
+            <Paper>
+                <form>
+                    <TextField
+                        type="email"
+                        label="Email"
+                    />
+                    <TextField
+                        label="login"
+                        placeholder=""
+                    />
+                    <Button />
+                </form>
+            </Paper>
+        )
+
+    }
+}
 
 export default Register
