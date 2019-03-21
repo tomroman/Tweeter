1fb7912c47209f27a5938bd172a228f851e9e29b
Thomas Roman <thomasroman32@yahoo.com>
2019-03-21 00:36:10 -0500

    created register.js file in validation folder. Uses if statement to ensure user inputs are appropriate. Also connected the validation file to the users.js file in routes folder.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,39 @@
 const router = require('express').Router()
 const User = require('../models/User')
-
+const bcrypt = require('bcryptjs')
+const validateRegisterInput = require('../validation/register')
 
 
 router.route('/register')
-    .get((req, res) => {
+    .post((req, res) => {
+        const { isValid, errors } = validateRegisterInput(req.body)
+
+        if (!isValid) {
+            return res.status(404).json(errors)
+        }
+
+        User.findOne({ email: req.body.email })
+            .then(user => {
+                if (user) {
+                    errors.email = 'Email was used!'
+                    return res.status(404).json(errors)
+                }
+
+                bcrypt.genSalt(10, function (err, salt){
+                    bcrypt.hash(req.body.password, salt, function (err, hash) {
+                        const newUser = new User ({
+                            email: req.body.email, 
+                            login: req.body.login, 
+                            password: hash
+                        })
+
+                        newUser.save()
+                            .then(newUser => res.json(newUser))
+                            .catch(err => console.log(err))
+                            
+                    })
+                })
+            })
         
     })
 
