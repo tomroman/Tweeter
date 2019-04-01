55ebc991ddc68e924a1cf04b4910bc3fe74550e4
Thomas Roman <thomasroman32@yahoo.com>
2019-03-31 23:30:14 -0500

    changed Header into a class and imported Component into Header.js file.

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,33 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 
 import { Link } from 'react-router-dom'
-import { withStyles} from '@material-ui/core/styles'
+import { withStyles } from '@material-ui/core/styles'
 import { connect } from 'react-redux'
 
 const styles = {
     root: {
-        flexGrow: 1 
-    }, 
+        flexGrow: 1
+    },
     logo: {
         color: '#fff',
-        fontSize: 30, 
+        fontSize: 30,
         TextTransform: 'uppercase'
     }
 }
 
-const Header = ({ classes }) => {
-    return (
-        <div className={classes.root}>
-            <AppBar position="static" style={{ backgroundColor: '#480882'}}>
-                <Toolbar>
-                    <Link to="/" className={classes.logo}>Tweeter</Link> 
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
-} 
+class Header extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            anchorEl: null 
+        }
+    }
+    render() {
+        const { classes } =this.props;
+        return (
+            <div className={classes.root}>
+                <AppBar position="static" style={{ backgroundColor: '#480882' }}>
+                    <Toolbar>
+                        <Link to="/" className={classes.logo}>Tweeter</Link>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
+}
 
 const mapStateToProps = (state) => ({
     isAuthenticated: state.auth
