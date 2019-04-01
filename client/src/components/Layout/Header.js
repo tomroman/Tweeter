5d8dc688702eca9694351c22b76ae429096f444d
Thomas Roman <thomasroman32@yahoo.com>
2019-04-01 00:04:18 -0500

    added AccountCircle with material-ui in Header.js.

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -4,6 +4,7 @@ import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
+import AccountCircle from '@material-ui/icons/AccountCircle'
 
 import { Link } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles'
@@ -19,31 +20,52 @@ const styles = {
         TextTransform: 'uppercase'
     }
 }
-handleMenu = (event) => {this.setState({ anchorEl: event.target.currentTarget })} 
 
-handleColor = () => {this.setState({ anchorEl: null })}
 
 class Header extends Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.state = {
-            anchorEl: null 
+            anchorEl: null
         }
     }
-    render() {
-        const { classes } =this.props;
+    handleMenu = (event) => { this.setState({ anchorEl: event.target.currentTarget }) }
 
+    handleColor = () => { this.setState({ anchorEl: null })}
+
+    render() {
+        const { classes } = this.props;
+        const { anchorEl } = this.state
+        const open = Boolean(anchorEl)
         const authLinks = (
             <div>
+                <IconButton
+                    aria-owns={open ? 'menu' : undefined}
+                    aria-haspopup="true"
+                    color="inherit"
+                    onClick={this.handleMenu}
 
-            </div> 
+                >
+                    <AccountCircle />
+                </IconButton>
+                <Menu
+                    id="menu"
+                    open={open}
+                    anchorEl={anchorEl}
+                >
+                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+
+                    <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                </Menu>
+            </div>
         )
         return (
             <div className={classes.root}>
                 <AppBar position="static" style={{ backgroundColor: '#480882' }}>
                     <Toolbar>
                         <Link to="/" className={classes.logo}>Tweeter</Link>
-                        
+                        { authLinks }
+
                     </Toolbar>
                 </AppBar>
             </div>
