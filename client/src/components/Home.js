28ea4ccee7469841163ab674cefd89e063d22589
Thomas Roman <thomasroman32@yahoo.com>
2019-04-04 23:22:33 -0500

    Attempted to add react router dom to home.js file but ended up reverting it back

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+
 import ListPost from './Posts/ListPost'
 import Login from './Auth/Login'
 
 class Home extends Component {
+
     render () {
         const { isAuthenticated } = this.props
         return (
